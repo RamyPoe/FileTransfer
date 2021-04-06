ed88f567c9cd7886a656ad6d06a0acb47836fbee
RamyPoe <43919906+RamyPoe@users.noreply.github.com>
2021-04-06 12:39:45 -0400

    Add files via upload

---
+++ client.py
@@ -0,0 +1,45 @@
+#Sending file
+import socket, time
+import glob
+import os
+
+PORT = 6437
+
+#------------MAKE CONNECTION----------------------
+while True:
+    try:
+        IP = input('Enter Server IP> ')
+        ADDR = (IP, PORT)
+        time.sleep(1)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect(ADDR)
+        break
+    except:
+        print('IP Incorrect or client not ready to recieve...')
+
+print("CONNECTED")
+#---------FUCNTIONS/SETUP--------
+def send(msg):
+    message = msg.encode('utf-8')
+    client.send(message)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#------------------SEND FILE----------------------
+
+
+for file in glob.glob(dir_path + '/send/*.*'):
+    extension = file.split('.')[-1]
+    name = file.split(dir_path)[-1].split('/send\\')[-1]
+
+    myfile = open(file, 'rb')
+    fileInfo = myfile.read()
+    size = len(fileInfo)
+
+    #-----------SEND FILE-----------
+    print(f"EXNTENSION ---> {extension}, NAME ---> {name}, SIZE ---> {size}")
+    send(f"INFO; .{size}.,{name},") #For server side parsing
+
+    time.sleep(.2)
+    client.sendall(fileInfo)
+    print(f"SENT FILE\n\n")
+    time.sleep(0.5)
---
+++ server.py
@@ -0,0 +1,36 @@
+#Recieving file
+import socket
+import os
+
+#-----------------------SERVER---------------------------
+#CONSTANTS
+IP = socket.gethostbyname(socket.gethostname())
+PORT = 6437
+ADDR = (IP, PORT)
+#START SERVER
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+print(f"[SERVER] Running on {socket.gethostname()} at {IP}:{PORT}")
+#GET READY FOR CLIENT CONNECTION
+server.listen()
+conn, addr = server.accept()
+print(f"[CONNECTION] {addr}")
+
+#----------FUNCTIONS------------
+def send(msg):
+    message = msg.encode('utf-8')
+    server.send(message)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#----------------RECIEVE FILE-------------------
+while True:
+    msg = conn.recv(1024).decode('utf-8')
+    if msg.startswith('INFO'):
+        size = msg.split('INFO; ')[-1].split('.')[1]
+        name = msg.split('INFO; ')[-1].split(',')[1]
+        fileInfo = conn.recv(int(size))
+        f = open(f"{dir_path}/recieve/{name}", 'wb')
+        f.write(fileInfo)
+        f.close()
+        print(f"RECIEVED FILE {name}")
+input('\n\n\n\nPress Enter to close...')
